238cc9f440a83556dd19b0faa526283534ca89f1
repl.it user <replituser@example.com>
2020-11-13 09:53:59 +0000

    first update

--- Life.java
+++ Life.java
@@ -1,5 +1,8 @@
 public class Life implements ILife {
   
+  //public boolean[][] habitat;
+  public char[][] habitat;
+
   public static void main(String[] args) {
     Life l = new Life(new String[] {  "     ",
                                       "     ",
@@ -9,9 +12,9 @@ public class Life implements ILife {
     l = (Life) l.nextGeneration();
   }
 
-
   public Life() {
     nukeAll();
+    
   }
 
   public Life(String[] setup) {
@@ -20,35 +23,42 @@ public class Life implements ILife {
       for (int x = 0; x < setup[y].length(); x++)
         if (setup[y].charAt(x) != ' ')
           setAlive(x, y);
+        else setDead(x,y);
   }
 
 
   @Override
   public void nukeAll() {
-    // TODO Auto-generated method stub
+    if(habitat.length > 0) setNewHabitat(habitat.length, habitat[0].length);
+    else setNewHabitat(10, 10);
+  }
 
+  public void setNewHabitat(int width, int height){
+    for(int y = 0; y < height; y++){
+      for(int x = 0; x < width; x++){
+        setDead(x,y);
+      }
+    }
   }
 
   @Override
   public void setAlive(int x, int y) {
-    // TODO Auto-generated method stub
-
+    habitat[x][y] = '*';
   }
 
   @Override
   public void setDead(int x, int y) {
-    // TODO Auto-generated method stub
-
+    habitat[x][y] = ' ';
   }
 
   @Override
   public boolean isAlive(int x, int y) {
-    // TODO Auto-generated method stub
-    return false;
+    if(habitat[x][y] == '*') return true;
+    else return false;
   }
 
   @Override
-  public ILife nextGeneration() {
+  public Life nextGeneration() {
     // TODO Auto-generated method stub
     return null;
   }
